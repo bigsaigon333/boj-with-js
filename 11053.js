9c7af1e487c8d4e7aa78a4fefa405c99404380ce
bigsaigon333 <likepeppermint@gmail.com>
2021-01-13 17:12:21 +0900

    refactor: 11053번을 바킹독의 코드를 참조하여 리팩토링한다

--- 11053.js
+++ 11053.js
@@ -13,13 +13,11 @@ const dp = Array(N).fill(0);
 dp[0] = 1;
 
 for (let i = 1; i < N; i++) {
-  let maxCount = 0;
+  dp[i] = 1;
   for (let j = 0; j < i; j++) {
-    if (maxCount < dp[j] && num[j] < num[i]) {
-      maxCount = dp[j];
+    if (num[j] < num[i]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
     }
   }
-
-  dp[i] = maxCount + 1;
 }
 console.log(Math.max(...dp));
