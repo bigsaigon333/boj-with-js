dac0d0ccca7a8a02e921e0fa663b084d4ac4dc88
bigsaigon333 <likepeppermint@gmail.com>
2021-07-25 21:45:36 +0900

    chore: jest 도입

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
+    node: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: ["eslint:recommended", "prettier"],
-  plugins: [],
+  plugins: ["jest"],
   parserOptions: {
     ecmaVersion: 12,
+    sourceType: "module",
   },
 };
